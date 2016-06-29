3ed57f57065ce04a8491b5eb313b16a200820ddd
Espen Grimsgaard <espen@r8edge.no>
2016-06-29 23:16:55 +0200

    Add the actual countdown function

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -27,7 +27,7 @@ describe('CountdownForm', () => {
     var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
     var $el = $(ReactDOM.findDOMNode(countdownForm));
 
-    countdownForm.refs.seconds.value = '1d3';
+    countdownForm.refs.seconds.value = 'leet';
     TestUtils.Simulate.submit($el.find('form')[0]);
 
     expect(spy).toNotHaveBeenCalled();
