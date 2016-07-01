f57dc08ae32f4033e0f37bf4f29fd4ee25d18efc
Espen Grimsgaard <espen@r8edge.no>
2016-07-01 23:47:57 +0200

    Update status to stopped when count reaches zero

--- public/bundle.js
+++ public/bundle.js
@@ -25503,6 +25503,11 @@
 	    }
 	  },
 
+	  componentWillUnmount: function componentWillUnmount() {
+	    clearInterval(this.timer);
+	    this.timer = undefined;
+	  },
+
 	  startTimer: function startTimer() {
 	    var _this = this;
 
@@ -25511,6 +25516,9 @@
 	      _this.setState({
 	        count: newCount >= 0 ? newCount : 0
 	      });
+	      if (newCount === 0) {
+	        _this.setState({ countdownStatus: 'stopped' });
+	      }
 	    }, 1000);
 	  },
 
